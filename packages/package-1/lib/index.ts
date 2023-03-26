7f54afc880fa9dd0979370f70acd478a6773ced7
Jacob Lichty <jacobl@live.ca>
2023-03-26 17:36:22 -0400

    prettify

--- packages/package-1/lib/index.ts
+++ packages/package-1/lib/index.ts
@@ -1,13 +1,33 @@
 // <autogen>
-import IInterface, {  } from './interfaces/IInterface';
-import MyClass, {  } from './MyClass';
-import MySubfolderClass, {  } from './subfolder/MySubfolderClass';
-import exportdefaultanonymousarrowfunction, {  } from './utils/export-default-anonymous-arrow-function';
-import exportdefaultanonymousfunction, {  } from './utils/export-default-anonymous-function';
-import exportdefaultanonymousobject, {  } from './utils/export-default-anonymous-object';
-import exportdefaultnamedarrowfunction, {  } from './utils/export-default-named-arrow-function';
-import exportdefaultnamedobject, {  } from './utils/export-default-named-object';
-import myFunction, { myOtherFunction, myArrowFunction, myVariable, IMyInterface } from './utils/export-multiple-objects';
-import namedFunction, {  } from './utils/export-default-named-function';
-export { IInterface, MySubfolderClass, exportdefaultanonymousarrowfunction, exportdefaultanonymousfunction, exportdefaultanonymousobject, exportdefaultnamedarrowfunction, namedFunction, exportdefaultnamedobject, myFunction, myOtherFunction, myArrowFunction, myVariable, IMyInterface, MyClass };
+import IInterface from './interfaces/IInterface';
+import MyClass from './MyClass';
+import MySubfolderClass from './subfolder/MySubfolderClass';
+import exportdefaultanonymousarrowfunction from './utils/export-default-anonymous-arrow-function';
+import exportdefaultanonymousfunction from './utils/export-default-anonymous-function';
+import exportdefaultanonymousobject from './utils/export-default-anonymous-object';
+import exportdefaultnamedarrowfunction from './utils/export-default-named-arrow-function';
+import exportdefaultnamedobject from './utils/export-default-named-object';
+import myFunction, {
+  myOtherFunction,
+  myArrowFunction,
+  myVariable,
+  IMyInterface,
+} from './utils/export-multiple-objects';
+import namedFunction from './utils/export-default-named-function';
+export {
+  IInterface,
+  MySubfolderClass,
+  exportdefaultanonymousarrowfunction,
+  exportdefaultanonymousfunction,
+  exportdefaultanonymousobject,
+  exportdefaultnamedarrowfunction,
+  namedFunction,
+  exportdefaultnamedobject,
+  myFunction,
+  myOtherFunction,
+  myArrowFunction,
+  myVariable,
+  IMyInterface,
+  MyClass,
+};
 // </autogen>
