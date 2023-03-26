aa8f7dfc5f9833132b8391b374a52d2c71042eea
Jacob Lichty <jacobl@live.ca>
2023-03-26 19:03:37 -0400

    update deps

--- packages/package-1/lib/index.ts
+++ packages/package-1/lib/index.ts
@@ -1,65 +1,20 @@
 // <autogen>
-import IInterface from './interfaces/IInterface';
-import IInterfaceWithGeneric from './interfaces/IInterfaceWithGeneric';
-import MyClass from './MyClass';
-import MyGenericSubfolderClass from './subfolder/MyGenericSubfolderClass';
-import MySubfolderClass from './subfolder/MySubfolderClass';
-import exportdefaultanonymousarrowfunction from './utils/export-default-anonymous-arrow-function';
-import exportdefaultanonymousfunction from './utils/export-default-anonymous-function';
-import exportdefaultanonymousgenericarrowfunction from './utils/export-default-anonymous-generic-arrow-function';
-import exportdefaultanonymousgenericfunction from './utils/export-default-anonymous-generic-function';
-import exportdefaultanonymousobject from './utils/export-default-anonymous-object';
-import exportdefaultnamedarrowfunction from './utils/export-default-named-arrow-function';
-import exportdefaultnamedobject from './utils/export-default-named-object';
-import myFunction, {
-  myOtherFunction,
-  myGenericFunction,
-  myArrowFunction,
-  myGenericArrowFunction,
-  myVariable,
-  myTypedVariable,
-  MyBasicClass,
-  MyGenericClass,
-  MySubClass,
-  IMyInterface,
-  IMyGenericInterface,
-  IMyOtherInterface,
-  MySpecialClass,
-  MyVerySpecialClass,
-} from './utils/export-multiple-objects';
-import namedFunction from './utils/export-default-named-function';
-import namedGenericFunction from './utils/export-default-named-generic-function';
-export {
-  IInterface,
-  IInterfaceWithGeneric,
-  MyGenericSubfolderClass,
-  MySubfolderClass,
-  exportdefaultanonymousarrowfunction,
-  exportdefaultanonymousfunction,
-  exportdefaultanonymousgenericarrowfunction,
-  exportdefaultanonymousgenericfunction,
-  exportdefaultanonymousobject,
-  exportdefaultnamedarrowfunction,
-  namedFunction,
-  namedGenericFunction,
-  exportdefaultnamedobject,
-  myFunction,
-  myOtherFunction,
-  myGenericFunction,
-  myArrowFunction,
-  myGenericArrowFunction,
-  myVariable,
-  myTypedVariable,
-  MyBasicClass,
-  MyGenericClass,
-  MySubClass,
-  IMyInterface,
-  IMyGenericInterface,
-  IMyOtherInterface,
-  MySpecialClass,
-  MyVerySpecialClass,
-  MyClass,
-};
+import IInterface, {  } from './interfaces/IInterface';
+import IInterfaceWithGeneric, {  } from './interfaces/IInterfaceWithGeneric';
+import MyClass, {  } from './MyClass';
+import MyGenericSubfolderClass, {  } from './subfolder/MyGenericSubfolderClass';
+import MySubfolderClass, {  } from './subfolder/MySubfolderClass';
+import exportdefaultanonymousarrowfunction, {  } from './utils/export-default-anonymous-arrow-function';
+import exportdefaultanonymousfunction, {  } from './utils/export-default-anonymous-function';
+import exportdefaultanonymousgenericarrowfunction, {  } from './utils/export-default-anonymous-generic-arrow-function';
+import exportdefaultanonymousgenericfunction, {  } from './utils/export-default-anonymous-generic-function';
+import exportdefaultanonymousobject, {  } from './utils/export-default-anonymous-object';
+import exportdefaultnamedarrowfunction, {  } from './utils/export-default-named-arrow-function';
+import exportdefaultnamedobject, {  } from './utils/export-default-named-object';
+import myFunction, { myOtherFunction, myGenericFunction, myArrowFunction, myGenericArrowFunction, myVariable, myTypedVariable, MyBasicClass, MyGenericClass, MySubClass, IMyInterface, IMyGenericInterface, IMyOtherInterface, MySpecialClass, MyVerySpecialClass } from './utils/export-multiple-objects';
+import namedFunction, {  } from './utils/export-default-named-function';
+import namedGenericFunction, {  } from './utils/export-default-named-generic-function';
+export { IInterface, IInterfaceWithGeneric, MyGenericSubfolderClass, MySubfolderClass, exportdefaultanonymousarrowfunction, exportdefaultanonymousfunction, exportdefaultanonymousgenericarrowfunction, exportdefaultanonymousgenericfunction, exportdefaultanonymousobject, exportdefaultnamedarrowfunction, namedFunction, namedGenericFunction, exportdefaultnamedobject, myFunction, myOtherFunction, myGenericFunction, myArrowFunction, myGenericArrowFunction, myVariable, myTypedVariable, MyBasicClass, MyGenericClass, MySubClass, IMyInterface, IMyGenericInterface, IMyOtherInterface, MySpecialClass, MyVerySpecialClass, MyClass };
 // </autogen>
 const hello = 2;
 console.log(hello);
