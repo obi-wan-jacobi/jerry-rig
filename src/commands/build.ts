c9b41544c0afc2fd25f81158af6ce2842e704782
Jacob Lichty <jacobl@live.ca>
2023-05-10 18:14:00 -0400

    1.0.15

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -5,7 +5,8 @@ const args = process.argv.slice(2);
 const cwd = args[0];
 const packageNames = args.length > 0 ? args[1].split(',').filter((x) => x !== 'undefined') : [];
 
-new CommandHandler({ cmd: buildPackageIndex, cwd, packageNames }).invoke()
+new CommandHandler({ cmd: buildPackageIndex, cwd, packageNames })
+  .invoke()
   .then(() => {
     return new CommandHandler({ cmd: 'npm run build', cwd, packageNames }).invoke();
   })
