9147bb83548727de8a6b9e205ed62dc2bb038aee
Jacob Lichty <jacobl@live.ca>
2023-05-10 18:30:25 +0000

    add windows/linux capability

--- src/shell/spawnChildProcessInSameShell.ts
+++ src/shell/spawnChildProcessInSameShell.ts
@@ -1,8 +1,23 @@
 import { spawn } from 'child_process';
 
 export default function spawnChildProcessInSameShell({ cmd, cwd }: { cmd: string; cwd: string }): Promise<void> {
+  if (process.platform === 'win32') {
+    return new Promise((resolve, reject) => {
+      spawn('cmd', ['/c', cmd], {
+        cwd,
+        shell: true,
+        stdio: 'inherit',
+      })
+        .addListener('close', (code: number) => {
+          code === 0 ? resolve() : reject();
+        })
+        .addListener('error', () => {
+          reject();
+        });
+    });
+  }
   return new Promise((resolve, reject) => {
-    spawn('cmd', ['/c', cmd], {
+    spawn(cmd, {
       cwd,
       shell: true,
       stdio: 'inherit',
